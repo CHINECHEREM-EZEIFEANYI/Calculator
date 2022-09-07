fb9b164072f3d044378775a106089acf7f34a1d6
EJIKE <perpetualezeifeanyi@gmail.com>
2022-09-07 23:28:42 +0100

    functionality

--- index.js
+++ index.js
@@ -5,3 +5,33 @@ const deleteButton = document.querySelector('[delete]')
 const allClearButton = document.querySelector('[all-clear]')
 const previousOperandTextElement = document.querySelector('[previous-operand]')
 const currentOperandTextElement = document.querySelector('[current-operand]')
+
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
+    }
+};
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+clear() {
+    this.currentOperand = ''
+    this.previousOperand = ''
+    this.operation = undefined
+}
+
+delete() {
+}
+
+displayNumber(number) {
+}
+
+chooseOperation(operation) {
+}
+
+compute() {
+}
+
+updateDisplay() {
+}

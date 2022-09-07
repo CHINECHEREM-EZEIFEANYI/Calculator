b78f7734aeeaad7ba2a2ea3b6a9832feae3fd322
EJIKE <perpetualezeifeanyi@gmail.com>
2022-09-07 23:16:52 +0100

    functinalities

---
+++ index.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll('[number]')
+const operationButtons = document.querySelectorAll('[operation]')
+const equalsButton = document.querySelector('[equals]')
+const deleteButton = document.querySelector('[delete]')
+const allClearButton = document.querySelector('[all-clear]')
+const previousOperandTextElement = document.querySelector('[previous-operand]')
+const currentOperandTextElement = document.querySelector('[current-operand]')

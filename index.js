180a6c54061b6d84094814aa4dfe820bf2c15a8e
EJIKE <perpetualezeifeanyi@gmail.com>
2022-09-14 04:57:40 +0100

    cleanup

--- index.js
+++ index.js
@@ -44,7 +44,41 @@ deleteButton.addEventListener('click', button => {
     calculator.updateDisplay()
 })
 
+document.addEventListener('keydown', function (event) {
+    let patternForNumbers = /[0-9]/g;
+    let patternForOperators = /[+\-*\/]/g
+    if (event.key.match(patternForNumbers)) {
+        event.preventDefault();
+        calculator.appendNumber(event.key)
+        calculator.updateDisplay()
+    }
+    if (event.key === '.') {
+        event.preventDefault();
+        calculator.appendNumber(event.key)
+        calculator.updateDisplay()
+    }
+    if (event.key.match(patternForOperators)) {
+        event.preventDefault();
+        calculator.chooseOperation(event.key)
+        calculator.updateDisplay()
+    }
+    if (event.key === 'Enter' || event.key === '=') {
+        event.preventDefault();
+        calculator.compute()
+        calculator.updateDisplay()
+    }
+    if (event.key === "Backspace") {
+        event.preventDefault();
+        calculator.delete()
+        calculator.updateDisplay()
+    }
+    if (event.key == 'Delete') {
+        event.preventDefault();
+        calculator.clear()
+        calculator.updateDisplay()
+    }
 
+});
 clear() ={
     this.currentOperand = ''
     this.previousOperand = ''
